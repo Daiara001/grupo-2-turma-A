14e25db0fac699e46962ceaf06abfe824e0dea4a
Marcos Ribeiro <ass.marcosribeiro@gmail.com>
2021-11-22 22:04:51 -0300

    Update jogo.java

--- src/jogo.java
+++ src/jogo.java
@@ -4,7 +4,7 @@ public class jogo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hahahahaahahucxhdcviubfvfbviufbviufbnfbvfiup");
+		System.out.println("teste");
 
 	}
 
